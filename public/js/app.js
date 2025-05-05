b44f3b6ef55ce9bb059d10da4fdbf33a1f87dbc9
yahya-mousair <yahyamoussair05@gmail.com>
2025-05-05 20:25:32 +0100

    complete v01

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,38 @@
-let container = document.querySelector(".container");
-let calculatorRows = document.querySelector(".calculator-rows");
-let display = document.querySelector("input");
+let btnContainer = document.querySelectorAll(".button-container");
+let rowsContainer = document.querySelectorAll(".rows-calculator");
+let display = document.querySelector(".display");
+let symbole = [".", "+", "-", "*", "/", "=", "^2", ")"];
 
-container.addEventListener("click", (e) => {
-  if (e.target.nodeName == "BUTTON") {
-    if (e.target.textContent == "C") {
-      clearDisplay();
-    } else if (e.target.textContent == "=") {
-      calculate();
-    }
-    writeInInput(e.target.textContent);
-  }
-});
+for (let i = 0; i < rowsContainer.length; i++) {
+  rowsContainer[i].childNodes.forEach((e) => {
+    e.addEventListener("click", () => {
+      if (e.textContent == "DEL") {
+        display.value = clearLastChar(display.value);
+      } else if (e.textContent == "C") {
+        clearDisplay(display);
+      } else if (e.textContent == "=") {
+        calculate(display);
+      } else {
+        display.value += e.textContent;
+      }
+    });
+  });
+}
 
-const writeInInput = (value) => {
-  display.value += value;
+const clearLastChar = (display) => {
+  return display.substring(0, display.length - 1);
 };
 
-const clearDisplay = () => {
+const clearDisplay = (display) => {
   display.value = "";
 };
 
-const calculate = () => {
+const calculate = (display) => {
   try {
-    let calculation = math.evaluate(display.value);
-    display.value = calculation
+    let calculate = math.evaluate(display.value);
+    return (display.value = calculate);
   } catch {
-    display.value = "Invalide operation";
+    display.value = "";
+    alert("invalid format");
   }
 };
